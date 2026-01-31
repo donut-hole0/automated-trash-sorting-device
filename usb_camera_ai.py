0a47bd06d5bcbeb1cd646fd09382486aabc20b09
ShouryaP9 <163233738+ShouryaP9@users.noreply.github.com>
2026-01-30 19:33:02 -0800

    Added No AI Computer Vision Detection Function

--- usb_camera_ai.py
+++ usb_camera_ai.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import tflite_runtime.interpreter as tflite
+import time
 
 #loading model
 interpreter = tflite.Interpreter(model_path="model.tflite")
@@ -17,6 +18,35 @@ if not cap.isOpened():
     print("camera not found")
     exit()
 print("Camera opened successfully")
+
+#No AI Computer Vision Object Detection Function (Shourya's Code)
+# U CAN CALL THIS FUNCTION WHEREVER U WANT, ITS NOT CURRENTLY BEING CALLED
+def detectObject():
+
+    ret1, frame1 = cap.read()
+    frame1 = frame1[100:500, 100:500]
+    time.sleep(0.3)
+    ret2, frame2 = cap.read()
+    frame2 = frame2[100:500, 100:500]
+    if ret1 and ret2:
+        gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+        blurredGray1 = cv2.GaussianBlur(gray1, (11, 11), 0)
+        gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+        blurredGray2 = cv2.GaussianBlur(gray2, (11, 11), 0)
+        diff = cv2.absdiff(blurredGray1, blurredGray2)
+        _, threshedDiff = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+        contours, _ = cv2.findContours(threshedDiff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        frame2Copy = frame2.copy()
+        count = 0
+        for contour in contours:
+            if cv2.contourArea(contour) >= 600: #change to 500 or 550? Or maybe 650?
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(frame2, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                count+=1
+                print(count)
+        cv2.imshow("cam", frame2)
+        cv2.imshow("contours", cv2.drawContours(frame2Copy, contours, -1, (0, 255, 0), 2))
+        
 
 while True:
     ret, frame = cap.read()
